1d25a20158efa4a8d3ecdc8b1b80d1dcfdf9cc08
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-09-21 08:45:34 +0700

    pembuatan tabel cuti

---
+++ app/Models/CutiPegawai.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CutiPegawai extends Model
+{
+    use HasFactory;
+}
