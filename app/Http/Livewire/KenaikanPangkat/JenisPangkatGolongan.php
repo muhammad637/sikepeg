7063916fa802d6e535901bd48a568843d0b7a9de
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-11-04 09:22:38 +0700

    testing

--- app/Http/Livewire/KenaikanPangkat/JenisPangkatGolongan.php
+++ app/Http/Livewire/KenaikanPangkat/JenisPangkatGolongan.php
@@ -18,37 +18,40 @@ class JenisPangkatGolongan extends Component
     public $resultGolongan = [];
     public $resultPangkat = [];
     public $jabatan;
-    public function mount(){
-    //   $this->pegawai = old('pegawai', null);
-    //   if($this->pegawai){
-    //     $this->resultGolongan =
-    //         Golongan::where('jenis', $this->pegawai->status_tipe)->orderBy('nama_golongan','asc')->get();
-    //     $this->resultPangkat =
-    //         Pangkat::orderBy('nama_pangkat', 'asc')->get();
-    //         $this->jabatan = old('jabatan',$this->pegawai->jabatan);
-    //     } 
-    //     $this->pangkat_id = old('pangkat_id', null);
-    //     $this->golongan_id = old('golongan_id', null);
-    //     $this->nama_golongan = old('nama_golongan', null);
-    //     $this->nama_pangkat = old('nama_pangkat', null);
-      
-    //     $this->jabatan = old('jabatan', null);
+    public function mount()
+    {
+        $this->pegawai = old('pegawai', null);
+        if ($this->pegawai) {
+            $this->resultGolongan =
+                Golongan::where('jenis', $this->pegawai->status_tipe)->orderBy('nama_golongan', 'asc')->get();
+            $this->resultPangkat =
+                Pangkat::orderBy('nama_pangkat', 'asc')->get();
+            $this->jabatan = old('jabatan', $this->pegawai->jabatan);
+        }
+        $this->pangkat_id = old('pangkat_id', null);
+        $this->golongan_id = old('golongan_id', null);
+        $this->nama_golongan = old('nama_golongan', null);
+        $this->nama_pangkat = old('nama_pangkat', null);
+
+        $this->jabatan = old('jabatan', null);
     }
-    public function updatedPegawai($value){
+    public function updatedPegawai($value)
+    {
         $pegawai = Pegawai::find($value);
-        if($pegawai){
+        if ($pegawai) {
             $this->status_tipe = $pegawai->status_tipe ?? 'pns';
             $this->resultGolongan = Golongan::where('jenis', $pegawai->status_tipe)->get();
             $this->resultPangkat = Pangkat::all();
         }
     }
-    
-    
-    
-    
+
+
+
+
     public function render()
     {
         return view('livewire.kenaikan-pangkat.jenis-pangkat-golongan');
     }
+    
     
 }
