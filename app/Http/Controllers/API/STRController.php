786924a66f20017ce764dcb8183495ff8a4a8269
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2024-07-23 20:10:37 +0700

    str api

--- app/Http/Controllers/API/STRController.php
+++ app/Http/Controllers/API/STRController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers\API;
 
 use App\Models\STR;
@@ -11,38 +10,33 @@ use Yaza\LaravelGoogleDriveStorage\Gdrive;
 
 class STRController extends Controller
 {
-    //
-
     public function index()
     {
         $user = auth()->user();
         $str = STR::where('pegawai_id', $user->id)->orderBy('updated_at', 'desc')->get();
-        return response()->json(
-            [
-                'message' => "Data STR Pegawai berhasil Di tamplikan",
-                'status' => 'success',
-                'data' => $str,
-            ],
-            200
-        );
+        return response()->json([
+            'message' => "Data STR Pegawai berhasil ditampilkan",
+            'status' => 'success',
+            'data' => $str,
+        ], 200);
     }
 
     public function store(Request $request)
     {
-        //
-        // return $request->all();
         try {
             $validatedData = $request->validate([
                 'no_str' => 'required',
                 'penerbit_str' => 'required',
-                'tanggal_terbit_str' => 'required',
+                'tanggal_terbit_str' => 'required|date',
                 'no_sertikom' => 'required',
                 'kompetensi' => 'required',
-                'masa_berakhir_str' => 'required',
+                'masa_berakhir_str' => 'required|date',
                 'link_str' => 'required|mimes:pdf',
             ]);
+
             $fileName = time() . '_' . md5(uniqid()). '.' . $request->file('link_str')->getClientOriginalExtension();
             Gdrive::put('dokumen/str/' . $fileName, $request->file('link_str'));
+
             $str = STR::create([
                 'pegawai_id' => auth()->user()->id,
                 'no_str' => $request->no_str,
@@ -54,20 +48,21 @@ class STRController extends Controller
                 'masa_berakhir_str' => $request->masa_berakhir_str,
                 'link_str' => $fileName
             ]);
-            $notif = Notifikasi::notif('str', 'data STR pegawai ' . $str->pegawai->nama_lengkap . ' berhasil  dibuat oleh ' . auth()->user()->name, 'bg-success', 'fas fa-folder-plus');
+
+            $notif = Notifikasi::notif('str', 'Data STR pegawai ' . $str->pegawai->nama_lengkap . ' berhasil dibuat oleh ' . auth()->user()->name, 'bg-success', 'fas fa-folder-plus');
             $createNotif = Notifikasi::create($notif);
             $createNotif->admin()->sync(Admin::adminId());
             $createNotif->pegawai()->attach($str->pegawai->id);
 
             return response()->json([
                 'status' => 'success',
-                'message' => 'data str berhasil di buat',
+                'message' => 'Data STR berhasil dibuat',
                 'data' => $str
             ], 200);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'data error : ' . $th->getMessage(),
+                'message' => 'Data error: ' . $th->getMessage(),
                 'data' => $th->getMessage()
             ], 200);
         }
